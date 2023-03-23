0ce66d43044d64b4c271b9f6ccccc5acf0c0a7e8
paulchrisc <128294135+paulchrisc@users.noreply.github.com>
2023-03-23 03:45:40 -0700

    followed along the video only

--- script.js
+++ script.js
@@ -65,6 +65,19 @@ const firstName = 'Jonas';
 const lastName = 'Schmedtmann';
 console.log(firstName + ' ' + lastName);
 
+//Assignment Operators
+
 let x = 10 + 5;
-x += 10;
-console.log(x)
+x += 10; //x = x + 10 = 25
+x *= 4; // x = x*4 = 100
+x++; // x = x + 1
+x--; //x= x - 1
+x--;
+console.log(x);
+
+// Comparison Operators
+
+console.log(ageJonas > ageSarah); // >, <, >=, <==
+console.log(ageSarah >= 18);
+
+console.log(now - 1991 > now - 2018);
