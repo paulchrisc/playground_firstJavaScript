8b270bb85fd212b81e2e5c8769996fe401358c31
paulchrisc <128294135+paulchrisc@users.noreply.github.com>
2023-03-23 03:11:32 -0700

    First Commit

---
+++ script.js
@@ -0,0 +1,53 @@
+/*let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName)
+
+
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991
+console.log(typeof year);
+
+console.log(typeof null);
+
+*/
+
+//reassigning / mutated the var
+
+let age = 30
+age = 31;
+
+//always use const unless you are sure that the variable will change
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+
+
+
+var job = 'programmer';
+job = 'teacher';
